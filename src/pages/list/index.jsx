e0f31a7ad0c14ab7c8748142ec3e32e82c3cf568
Toby <toby@Tobyui-MacBookPro.local>
2022-02-01 01:45:58 +0900

    Fea: Add book card UI 추가

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,21 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './list.module.css';
 
 function List() {
   const contentListRef = useRef();
+  const [books, setBooks] = useState([]);
 
   const scrollingContent = type => {
     switch (type) {
       case 'prev':
-        contentListRef.current.style.transform = 'translate(-100%, 0)';
+        contentListRef.current.style.transform = 'translate(100%, 0)';
         break;
       case 'next':
-        contentListRef.current.style.transform = 'translate(100%, 0)';
+        contentListRef.current.style.transform = 'translate(-100%, 0)';
         break;
       default:
         console.warn('input parameter');
     }
   };
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/bookList.json')
+      .then(res => res.json())
+      .then(({ list }) => setBooks(list))
+      .catch(new Error(''));
+  }, []);
 
   return (
     <main className={styles.container}>
@@ -27,31 +35,52 @@ function List() {
             onClick={() => scrollingContent('prev')}
             aria-hidden
           >
-            btn
+            <i className={`fas fa-angle-left ${styles.angle}`} />
           </div>
           <div ref={contentListRef} className={styles.contentList}>
-            <li className={styles.content}>01</li>
-            <li className={styles.content}>02</li>
-            <li className={styles.content}>03</li>
-            <li className={styles.content}>04</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
-            <li className={styles.content}>00</li>
+            {books.map(book => (
+              <li className={styles.content} key={book.id}>
+                <img
+                  className={styles.thumbnail}
+                  src={book.thumbnail}
+                  alt="book"
+                />
+                <div className={styles.bookDescription}>
+                  <h2 className={styles.bookTitle}>{book.title}</h2>
+                  <div className={styles.bookExplanation}>
+                    <div className={styles.descriptionIcon}>
+                      <i className="fas fa-user-alt" />
+                    </div>
+                    {book.author}
+                  </div>
+                  <div className={styles.bookExplanation}>
+                    <div className={styles.descriptionIcon}>
+                      <i className="fas fa-book-open" />
+                    </div>
+                    {book.description}
+                  </div>
+                  <div className={styles.bookExplanation}>
+                    <div className={styles.descriptionIcon}>
+                      <i className="fas fa-calendar-alt" />
+                    </div>
+                    {book.published}
+                  </div>
+                  <div className={styles.bookExplanation}>
+                    <div className={styles.descriptionIcon}>
+                      <i className="fas fa-calendar-check" />
+                    </div>
+                    {book.readDate}
+                  </div>
+                </div>
+              </li>
+            ))}
           </div>
           <div
             className={styles.nextBtn}
             onClick={() => scrollingContent('next')}
             aria-hidden
           >
-            hello
+            <i className={`fas fa-angle-right ${styles.angle}`} />
           </div>
         </ul>
       </section>
