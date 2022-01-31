f7fe6b0ad80932fce90085930e9dc33c7f212ed0
Toby <toby@Tobyui-MacBookPro.local>
2022-01-31 11:17:57 +0900

    Add: theme btn Link 추가 및 transition 효과 추가

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from 'assets/images/test.webp';
 import styles from './landing.module.css';
 
@@ -7,18 +8,24 @@ function Landing() {
     <section className={styles.container}>
       <h1 className={styles.gretting}>000님 안녕하세요.</h1>
       <div className={styles.selectTab}>
-        <div className={styles.btn}>
-          <img className={styles.icon} src={img} alt="icon" />
-          <h2 className={styles.btnTitle}>Movie</h2>
-        </div>
-        <div className={styles.btn}>
-          <img className={styles.icon} src={img} alt="icon" />
-          <h2 className={styles.btnTitle}>Book</h2>
-        </div>
-        <div className={styles.btn}>
-          <img className={styles.icon} src={img} alt="icon" />
-          <h2 className={styles.btnTitle}>테마 추가</h2>
-        </div>
+        <Link to="/movie">
+          <div className={styles.btn}>
+            <img className={styles.icon} src={img} alt="icon" />
+            <h2 className={styles.btnTitle}>Movie</h2>
+          </div>
+        </Link>
+        <Link to="/book">
+          <div className={styles.btn}>
+            <img className={styles.icon} src={img} alt="icon" />
+            <h2 className={styles.btnTitle}>Book</h2>
+          </div>
+        </Link>
+        <Link to="/create_theme">
+          <div className={styles.btn}>
+            <img className={styles.icon} src={img} alt="icon" />
+            <h2 className={styles.btnTitle}>테마 추가</h2>
+          </div>
+        </Link>
       </div>
     </section>
   );
